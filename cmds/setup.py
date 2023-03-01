6bcd83d37efb068fc3a1e970402818f8a57bbcea
Kelzihar <kelzihar@gmail.com>
2023-03-01 16:12:28 -0700

    Setup finalizing

--- cmds/setup.py
+++ cmds/setup.py
@@ -1,29 +1,49 @@
 import discord
+from discord.utils import get
 from discord.ext import commands
 
-text = 'A command to quickly set up your collabrative environment. Input you connections separated by commas'
-@commands.hybrid_command(name = 'setup', with_app_command = True, description = text)
-async def setup(interaction: discord.Interaction, guild: discord.Guild, connections):
-    await interaction.response.send_message('Beginning your server setup!')
+client = discord.Client(intents = discord.Intents.all())
 
-    # Setup the categories to hold the channels
-    updates = guild.create_category('Updates')
-    tracker = guild.create_category('Schedule/Calendar')
-    ticket = guild.create_category('Tickets')
-    voice = guild.create_category('Voice Channels')
+class Setup(commands.Cog, name = "setup"):
+    def __init__(self, bot):
+        self.bot = bot
 
-    # Create the actual channels within the new categories
-    conList = connections.split(',')
-    for connect in conList:
-        channelName = connect.replace(' ')
-        updates.create_text_channel(name = channelName + '-updates')
+    text = 'A command to quickly set up your collabrative environment. Input you connections separated by commas'
+    @commands.hybrid_command(name = 'setup', with_app_command = True, description = text,)
+    @commands.has_permissions(administrator=True)
+    async def setup(self, ctx, connections):
 
-    tracker.create_text_channel(name = 'Team-member-scheduler')
-    tracker.create_text_channel(name = 'Upcoming meetings')
+        await ctx.send('Beginning your server setup!')
 
-    ticket.create_text_channel(name = 'Submi-a-ticket')
+        # Delete all initial categories, then delete the remaining channels
+        for channel in ctx.guild.channels:
+            await channel.delete()
+        for category in ctx.guild.categories:
+            await category.delete()
 
-    for i in range(1, 6):
-        voice.create_voice_channel(name = 'Voice ' + i)
 
-    await interaction.response.send_message('Your server is now setup!')
+        # Setup the categories to hold the channels
+        updates = await ctx.guild.create_category(name='Updates', position = 0, reason = 'New update channel')
+        tracker = await ctx.guild.create_category(name = 'Schedule/Calendar', position = 1, reason = 'New tracking channel')
+        ticket = await ctx.guild.create_category(name = 'Tickets', position = 2, reason = 'New ticketing channel')
+        voice = await ctx.guild.create_category(name = 'Voice Channels', position = 3, reason = 'New Voice Channels')
+
+        # Create the actual channels within the new categories
+        conList = connections.split(',')
+        general = await updates.create_text_channel(name = 'General')
+        for connect in conList:
+            channelName = connect.replace(' ', '')
+            await updates.create_text_channel(name = channelName + '-updates')
+
+        await tracker.create_text_channel(name = 'Team-member-scheduler')
+        await tracker.create_text_channel(name = 'Upcoming meetings')
+
+        await ticket.create_text_channel(name = 'Submit-a-ticket')
+
+        for i in range(1, 6):
+            await voice.create_voice_channel(name = 'Voice ' + str(i))
+
+        await general.send('Your server is now setup!')
+
+async def setup(bot):
+    await bot.add_cog(Setup(bot))
