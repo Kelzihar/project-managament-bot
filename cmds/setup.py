a612d2941b7f6c95c5c1e662cfb93617cfbc1566
Kelzihar <kelzihar@gmail.com>
2023-03-06 17:39:58 -0700

    Old work deletion

--- cmds/setup.py
+++ cmds/setup.py
@@ -75,36 +75,6 @@ class Setup(commands.Cog, name = "setup"):
         view.add_item(select)
         await ctx.send('Please select your connections!', view=view)
 
-#        await ctx.send('Beginning your server setup!')
-
-#        # Delete all initial categories, then delete the remaining channels
-#        for channel in ctx.guild.channels:
-#            await channel.delete()
-#        for category in ctx.guild.categories:
-#            await category.delete()
-
-
-#        # Setup the categories to hold the channels
-#        updates = await ctx.guild.create_category(name='Updates', position = 0, reason = 'New update channel')
-#        tracker = await ctx.guild.create_category(name = 'Schedule/Calendar', position = 1, reason = 'New tracking channel')
-#        ticket = await ctx.guild.create_category(name = 'Tickets', position = 2, reason = 'New ticketing channel')
-#        voice = await ctx.guild.create_category(name = 'Voice Channels', position = 3, reason = 'New Voice Channels')
-
-#        # Create the actual channels within the new categories
-#        general = await updates.create_text_channel(name = 'General')
-#        for connect in select.values:
-#            await updates.create_text_channel(name = connect + '-updates')
-
-#        await tracker.create_text_channel(name = 'Team-member-scheduler')
-#        await tracker.create_text_channel(name = 'Upcoming meetings')
-
-#        await ticket.create_text_channel(name = 'Submit-a-ticket')
-
-#        for i in range(1, 6):
-#            await voice.create_voice_channel(name = 'Voice ' + str(i))
-
-#        await general.send('Your server is now setup!')
-
     @setup.error
     async def setp_error(ctx, error):
         if isinstance(error, commands.CheckFailure):
